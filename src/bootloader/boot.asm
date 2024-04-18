03002d50dfd13b135ad94f409a5446c1c0edd10f
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-04-17 19:39:45 -0500

    Remove magic decklist and replace with boot.asm

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,65 +1,210 @@
-Commander
-1 Arvad, Weatherlight Smuggler (Y23) 20
+ORG 0x7C00
+BITS 16
 
-Deck
-1 Hopeless Nightmare (WOE) 95
-20 Plains (MKM) 278
-1 Cerebral Confiscation (MKM) 81
-1 Intellect Devourer (HBG) 162
-20 Swamp (MKM) 282
-1 Sorin the Mirthless (VOW) 131
-1 Necrosquito (ONE) 100
-1 Isildur's Fateful Strike (LTR) 91
-1 Phyrexian Rager (DMU) 99
-1 Scathe Zombies (ANB) 59
-1 Midnight Scavengers (SIR) 123
-1 Petrify (LCI) 30
-1 Grafted Butcher (MOM) 109
-1 Krovikan Scoundrel (ANB) 50
-1 Sweettooth Witch (WOE) 111
-1 Tithing Blade (LCI) 128
-1 Gixian Recycler (Y23) 27
-1 Vraan, Executioner Thane (ONE) 114
-1 Deadly Derision (MOM) 99
-1 Final Flourish (MOM) 104
-1 Ichor Drinker (MOM) 111
-1 Throne of the Grim Captain (LCI) 266
-1 Incriminate (SNC) 84
-1 Benalish Sleeper (DMU) 8
-1 Lonely End (Y23) 6
-1 Prison Sentence (BRO) 21
-1 Samite Herbalist (DMU) 31
-1 Boon of Safety (SNC) 4
-1 Join the Dead (LCI) 110
-1 Murder (ANB) 53
-1 Shrine Steward (NEO) 259
-1 Unlikely Aid (ANB) 64
-1 Wicked Visitor (WOE) 118
-1 Raise Dead (ANB) 56
-1 March Toward Perfection (Y23) 9
-1 Lembas (LTR) 243
-1 Vampiric Rites (WOT) 37
-1 Snarlfang Vermin (Y24) 12
-1 Snarling Gorehound (MKM) 105
-1 Sanitarium Skeleton (ANB) 57
-1 Sam's Desperate Rescue (LTR) 105
-1 Leering Onlooker (MKM) 91
-1 Sepulcher Ghoul (HBG) 166
-1 Deadly Dispute (HBG) 150
-1 Cruel Cut (ANB) 47
-1 Ashnod's Intervention (BRO) 85
-1 Phyrexian Missionary (DMU) 27
-1 Annihilating Glare (ONE) 80
-1 Ashnod, Flesh Mechanist (BRO) 84
-1 Cult Conscript (DMU) 88
-1 Vanquish the Weak (MOM) 129
-1 Unseal the Necropolis (MOM) 128
-1 Chain Devil (HBG) 149
-1 Vampire Spawn (HBG) 172
-1 Savage Gorger (ANB) 58
-1 Hero's Downfall (VOW) 120
-1 Graf Rats (SIR) 115
-1 Disfigure (BRO) 91
-1 Extract a Confession (MKM) 84
-1 Planar Disruption (ONE) 28
-1 Flitting Guerrilla (MOM) 105
+%define ENDL 0x0D, 0x0A
+
+; FAT12 HEADER
+
+jmp short start
+nop
+
+bdb_oem                         db 'MSWIN4.1'
+bdb_bytes_per_sector            dw 512
+bdb_sectors_per_cluster         db 1
+bdb_reserved_sectors            dw 1
+bdb_fat_count                   db 2
+bdb_dir_entries_count           dw 0E0h
+bdb_total_sectors               dw 2880
+bdb_media_descriptor_type       db 0F0h
+bdb_sector_per_fat              dw 9
+bdb_sectors_per_track           dw 18
+bdb_heads                       dw 2
+bdb_hidden_sectors              dd 0
+bdb_large_sector_count          dd 0
+
+; EXTENDED BOOT SECTOR
+
+ebr_drive_number        db 0
+                        db 0
+ebr_signature           db 29h
+ebr_volume_id           db 12h, 34h, 56h, 78h
+ebr_volume_label        db 'SLAY       '
+ebr_system_id           db 'FAT12   '
+
+; CODE SEGMENT NOT SEGMENT
+
+start:
+    jmp main
+
+main:
+    ; Initializing data segments
+    mov ax, 0                           ; We can not write to sd/es directly
+    mov ds, ax
+    mov es, ax
+
+    ; Set up stack
+    mov ss, ax
+    mov sp, 0x7C00                      ; Stack goes downward from where we are in memory
+
+    mov [ebr_drive_number], dl
+
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7E00
+    call disk_read
+
+    mov si, msg_hello
+    call puts
+
+    hlt
+
+; ERROR HANDLERS
+floppy_error:
+    mov  si, msg_read_failed
+    call puts
+    jmp  wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h
+    jmp 0FFFFh:0
+    hlt
+
+.halt:
+    cli
+    hlt
+
+; FUNCTIONS
+
+;
+; PRINTING TO SCREEN
+;
+
+;
+; PARAMS:
+;   - si: Address of string
+; RETURNS:
+;   VOID
+;
+puts:
+    ;Saving the registers that we will modify
+    push si
+    push ax
+
+.loop:
+    lodsb                               ; Loads next char in al
+    or al, al                           ; verify is the next character null
+    jz .done
+
+    mov ah, 0x0E
+    mov bh, 0
+    int 10h
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
+
+;
+; DISK ROUTINES
+;
+
+;
+; CONVERTS AN LBA ADDRESS TO CHS ADDRESS
+; PARAMS:
+;   - ax: LBA address
+; RETURNS:
+;   - cx: [bits 0-5]: sector number
+;   - cx: [bits 6-15]: cylinder
+;   - dh: head
+;
+
+lba_to_chs:
+    push ax
+    push dx
+
+    xor dx, dx
+    div word [bdb_sectors_per_track]
+
+    inc dx
+    mov cx, dx
+
+    xor dx, dx
+    div word [bdb_heads]
+
+    mov dh, dl
+    mov ch, al
+    shl ah, 6
+    or  cl, al
+
+    pop ax
+    mov dl, al
+    pop ax
+    ret
+
+;
+; Reads sectors from a disk
+; Parameters:
+; - ax: LBA address
+; - cl: number of sectors to read (up to 128)
+; - dl: drive number
+; - es:bx: memory address where to store read data
+;
+disk_read:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx                             ; Numbers of sectors to read
+    call lba_to_chs
+    pop ax
+
+    mov ah, 02h
+    mov di, 3
+
+.retry:
+    pusha
+    stc
+    int 13h
+    jnc .done
+
+    ; read failed
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    ; after all attempts fail
+    jmp floppy_error
+
+.done:
+    popa
+
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
+
+msg_hello:      DB 'Hello World', ENDL, 0
+msg_read_failed DB 'Read from disk failed!', ENDL, 0
+
+;TIMES 510-($-$$) DB 0                   ; Fill with 0's to let last two bytes be signatues.
+;DW 0xAA55
